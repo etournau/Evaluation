6833fe009d568394b9d0cb4048188c1e4bd7328e
Yoann.d <deletrey@gmail.com>
2022-05-20 12:28:00 +0200

    Evaluation

---
+++ Assets/Scripts/SnakeController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SnakeController : MonoBehaviour
+{
+    private Vector2 _direction = Vector2.right;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+            _direction = Vector2.right;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            _direction = -Vector2.up;    
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            _direction = -Vector2.right; 
+        else if (Input.GetKey(KeyCode.UpArrow))
+            _direction = Vector2.up;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float minSpawnTime, maxSpawnTime;
+    public GameObject enemy;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnCoroutine());
+    }
+
+    private IEnumerator SpawnCoroutine()
+    {
+        while (true)
+        {
+            Spawn();
+            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
+        }
+    }
+
+    private void Spawn()
+    {
+       
+    }
+}
